10f45c78468a5ddc1cd22737b5be626b9a5546eb
Mirko7222 <20xgame02@gmail.com>
2020-03-31 22:49:43 +0200

    corretto

--- src/fattorino.java
+++ src/fattorino.java
@@ -4,6 +4,7 @@ public class fattorino extends lavoratore{
     public fattorino(String n, String c){
         super(n,c);
     }
+    @Override
     public double stipendio(int giorni)
     {   int guadagno;
         int rgiornaliera=50;
