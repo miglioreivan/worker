b3df3448a647a96ff178055a73157bdd847b1ff1
Mirko7222 <20xgame02@gmail.com>
2020-03-31 22:29:47 +0200

    prova

--- src/main.java
+++ src/main.java
@@ -6,19 +6,19 @@ public class main {
         double stipendio;
         lavoratore l1 = new lavoratore("Luca","Abete");
         stipendio=l1.stipendio(29);
-        System.out.println("Lo stipendi di "+l1.getCognome()+" è di: €"+stipendio);
+        System.out.println("Lo stipendio di "+l1.getCognome()+" è di: €"+stipendio);
 
         capo c1 = new capo("Giacchino","Belli");
         stipendio=c1.stipendio(29);
-        System.out.println("Lo stipendi di "+c1.getCognome()+" è di: €"+stipendio);
+        System.out.println("Lo stipendio di "+c1.getCognome()+" è di: €"+stipendio);
 
         fattorino f1 = new fattorino("Fred","Marcelli");
         stipendio=f1.stipendio(29);
-        System.out.println("Lo stipendi di "+f1.getCognome()+" è di: €"+stipendio);
+        System.out.println("Lo stipendio di "+f1.getCognome()+" è di: €"+stipendio);
 
         impiegato i1 = new impiegato("Mario","Blu");
         stipendio=i1.stipendio(29);
-        System.out.println("Lo stipendi di "+i1.getCognome()+" è di: €"+stipendio);
+        System.out.println("Lo stipendio di "+i1.getCognome()+" è di: €"+stipendio);
 
 
     }
